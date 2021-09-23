b9a298b8c3c22350b143eadfac5dd9133cdb3cb1
bendunhampdx <bendunhampdx@gmail.com>
2021-09-23 13:48:15 -0700

    get two tests passing to determine scrabble score
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>

--- ScrabbleScorer.Tests/ModelTests/ScoreTests.cs
+++ ScrabbleScorer.Tests/ModelTests/ScoreTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScrabbleScorer.Models;
+using System.Collections.Generic;
 using System;
 
 namespace ScrabbleScorer.Tests
@@ -8,17 +9,24 @@ namespace ScrabbleScorer.Tests
   public class ValueTests
   {
     [TestMethod]
-  public void Main_WillReturnValue_Int()
-  {
-    //Arrange
-    string letter = "A";
-    int value = 1;
-    //Act
-    Score Ben = new Score(letter, value);
-   
-
-    //Assert
-    Assert.AreEqual(letter, value);
-  }
+  public void Dictionary_InitializesValuesCorrectly()
+    {
+      Dictionary<string, int> letters = new Dictionary<string, int>(){{"b", 3}, {"e", 1}, {"n", 1}};
+      Assert.AreEqual(3, letters["b"]);
+      Assert.AreEqual(1, letters["e"]);
+      Assert.AreEqual(1, letters["n"]);
+    }
+      [TestMethod]
+    public void IsASplitArray_TakesInAWord_SplitsIntoCharArrays()
+    {
+      //Arrange
+      string userInput = "ben";
+      //Act
+      Dictionary<string, int> letters = new Dictionary<string, int>() {{"b", 3}, {"e", 1}, {"n", 1}};
+      //Assert
+      Assert.AreEqual(3, letters["b"]);
+      Assert.AreEqual(1, letters["e"]);
+      Assert.AreEqual(1, letters["n"]);
+    }
   }
 }
